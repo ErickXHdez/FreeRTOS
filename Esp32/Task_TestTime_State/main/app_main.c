38b4a27206071bad4115a95a1b136296da84d388
ErickXHdez <76678582+ErickXHdez@users.noreply.github.com>
2021-05-04 17:23:28 -0400

    Resume_Suspend_Delete Task2

--- Esp32/Task_TestTime_State/main/app_main.c
+++ Esp32/Task_TestTime_State/main/app_main.c
@@ -24,6 +24,14 @@ void task1(void *arg){
         printf("Ejecutando Task1\n");
         vTaskDelay(1000 / portTICK_PERIOD_MS);
         if(c==3){
+            vTaskSuspend(TaskHandle2);    
+            printf("Task2 fue suspendida\n");
+        }
+        if(c==5){
+            vTaskResume(TaskHandle2);
+            printf("Task2 iniciada\n");
+        }
+        if(c==7){
             vTaskDelete(TaskHandle2);    
             printf("Task2 fue eliminada\n");
         }
@@ -31,7 +39,7 @@ void task1(void *arg){
 
 }
 void task2(void *arg){
-    for (int i = 0; i < 5; i++)
+    for (int i = 0; i <= 5; i++)
     {
         printf("Ejecutando Task2\n");
         vTaskDelay(1000 / portTICK_PERIOD_MS);
