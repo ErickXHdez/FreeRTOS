138546eb842fd8ff38121a8150deb0a5f9d744e5
ErickXHdez <76678582+ErickXHdez@users.noreply.github.com>
2021-05-04 16:49:39 -0400

    Primer commit

---
+++ Esp32/Task_TestTime_State/main/app_main.c
@@ -0,0 +1,39 @@
+
+#include <stdio.h>
+#include "freertos/FreeRTOS.h"
+#include "freertos/task.h"
+#include "driver/gpio.h"
+#include "sdkconfig.h"
+
+TaskHandle_t TaskHandle1 = NULL;
+TaskHandle_t TaskHandle2 = NULL;
+
+void task1(void *arg);
+void task2(void *arg);
+
+void app_main(void){
+    xTaskCreate(task1,"task1",2048, NULL, 5, &TaskHandle1);
+    xTaskCreate(task2,"task2",2048, NULL, 5, &TaskHandle2);
+}
+
+void task1(void *arg){
+    int c=0;
+    while (1)
+    {
+        c++;
+        printf("Ejecutando Task1\n");
+        vTaskDelay(1000 / portTICK_PERIOD_MS);
+        if(c==3){
+            vTaskDelete(TaskHandle2);    
+            printf("Task2 fue eliminada\n");
+        }
+    }
+
+}
+void task2(void *arg){
+    for (int i = 0; i < 5; i++)
+    {
+        printf("Ejecutando Task2\n");
+        vTaskDelay(1000 / portTICK_PERIOD_MS);
+    }     
+}
