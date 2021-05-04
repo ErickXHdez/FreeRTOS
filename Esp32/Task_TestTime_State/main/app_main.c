69435ad73f79283aaf4448729de70c27b0374a75
ErickXHdez <76678582+ErickXHdez@users.noreply.github.com>
2021-05-04 18:02:17 -0400

    Se ancló la Task2 en el nucleo 1, se agregó conteo de tiempo y el estado de las tareas

--- Esp32/Task_TestTime_State/main/app_main.c
+++ Esp32/Task_TestTime_State/main/app_main.c
@@ -1,4 +1,3 @@
-
 #include <stdio.h>
 #include "freertos/FreeRTOS.h"
 #include "freertos/task.h"
@@ -13,33 +12,41 @@ void task2(void *arg);
 
 void app_main(void){
     xTaskCreate(task1,"task1",2048, NULL, 5, &TaskHandle1);
-    xTaskCreate(task2,"task2",2048, NULL, 5, &TaskHandle2);
+    xTaskCreatePinnedToCore(task2,"task2",2048, NULL, 5, &TaskHandle2, 1);//Anclar la ejucucion de la tarea al segundo núcleo
 }
 
 void task1(void *arg){
     int c=0;
     while (1)
     {
-        c++;
-        printf("Ejecutando Task1\n");
-        vTaskDelay(1000 / portTICK_PERIOD_MS);
-        if(c==3){
+        /*Estado de tareas expresadas en números
+        Running = 0
+        Ready = 1
+        Blocked = 2
+        Suspended = 3
+        Deleted = 4
+        Invalid = 5
+        */
+        c=xTaskGetTickCount();
+        printf("Ejecutando %s, tiempo %d\n", pcTaskGetTaskName(TaskHandle1), c);
+        if(c==300){
             vTaskSuspend(TaskHandle2);    
-            printf("Task2 fue suspendida\n");
+            printf("Task2 fue suspendida %d\n", eTaskGetState(TaskHandle2));
         }
-        if(c==5){
+        if(c==500){
             vTaskResume(TaskHandle2);
-            printf("Task2 iniciada\n");
+            printf("Task2 iniciada %d\n", eTaskGetState(TaskHandle2));
         }
-        if(c==7){
+        if(c==700){
             vTaskDelete(TaskHandle2);    
-            printf("Task2 fue eliminada\n");
+            printf("Task2 fue eliminada %d\n", eTaskGetState(TaskHandle2));
         }
+        vTaskDelay(1000 / portTICK_PERIOD_MS);
     }
-
 }
+
 void task2(void *arg){
-    for (int i = 0; i <= 5; i++)
+    for (int i = 0; i <= 10; i++)
     {
         printf("Ejecutando Task2\n");
         vTaskDelay(1000 / portTICK_PERIOD_MS);
